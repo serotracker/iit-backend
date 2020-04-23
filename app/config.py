a8350849c31e3087763f4cc5491c09578b2c1bf1
Simona Rocco <serocco@uwaterloo.ca>
2020-04-23 17:35:53 -0400

    implemented logic for airtable_scraper/records endpoint to return all records that meet inclusion criteria

--- app/config.py
+++ app/config.py
@@ -5,8 +5,10 @@ class ApiConfig:
     DEBUG = True
     FLASK_DEBUG = True
     APP_NAMESPACES = os.getenv('APP_NAMESPACES', ['healthcheck', 'airtable_scraper'])
-    AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY', 'yourkey')
-    AIRTABLE_BASE_NAME = os.getenv('AIRTABLE_BASE_NAME', 'basename')
+    AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+    AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
+    BASE_REQUEST_URL = "https://api.airtable.com/v0/{}/Rapid%20Review%20Data?filterByFormula=" \
+                       "%7BMeets+Inclusion+Criteria%3F%7D%3D%22Yes%22".format(AIRTABLE_BASE_ID)
 
 
 class ApiTestingConfig(ApiConfig):
--- app/namespaces/airtable_scraper/airtable_scraper_controller.py
+++ app/namespaces/airtable_scraper/airtable_scraper_controller.py
@@ -1,11 +1,13 @@
-from flask import jsonify
 from flask_restplus import Resource, Namespace
+
+from .airtable_scraper_service import get_all_records
 
 airtable_scraper_ns = Namespace('airtable_scraper', description='An endpoint for scraping airtable data.')
 
 
-@airtable_scraper_ns.route('/', methods=['GET'])
+@airtable_scraper_ns.route('/records', methods=['GET'])
 class AirtableScraper(Resource):
     @airtable_scraper_ns.doc('An endpoint for getting all records of airtable data.')
     def get(self):
-        return jsonify('This endpoint will eventually return all records of airtable data.')
+        records = get_all_records()
+        return records
