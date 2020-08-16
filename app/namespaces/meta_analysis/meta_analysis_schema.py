57b422edac9aa78168427d166457776b7cd2f426
Simona Rocco <serocco@uwaterloo.ca>
2020-08-15 23:00:31 -0400

    refactoring meta analysis endpoint to query from db instead of accepting records

--- app/namespaces/meta_analysis/meta_analysis_schema.py
+++ app/namespaces/meta_analysis/meta_analysis_schema.py
@@ -1,8 +1,13 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class MetaSchema(Schema):
-    records = fields.List(fields.Dict, required=True)
+    class StudyCountSchema(Schema):
+        filters = fields.Dict(
+            keys=fields.String(validate=validate.OneOf(["country"])),
+            values=fields.List(fields.String())
+        )
     aggregation_variable = fields.String()
     meta_analysis_transformation = fields.String()
     meta_analysis_technique = fields.String()
+
