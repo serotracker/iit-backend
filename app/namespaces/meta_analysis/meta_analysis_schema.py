3bdbeadf1f26c5a21b24ed1cec8c546413e7e6be
Austin Atmaja <austin.atmaja@gmail.com>
2020-08-17 17:19:28 -0400

    Add more filters to meta analysis

--- app/namespaces/meta_analysis/meta_analysis_schema.py
+++ app/namespaces/meta_analysis/meta_analysis_schema.py
@@ -3,7 +3,10 @@ from marshmallow import Schema, fields, validate
 
 class MetaSchema(Schema):
     filters = fields.Dict(
-        keys=fields.String(validate=validate.OneOf(["country"])),
+        keys=fields.String(validate=validate.OneOf(["country", "source_type", "overall_risk_of_bias",
+                                                    "study_status", "source_name", "population_group_name",
+                                                    "sex", "age_name", "isotypes_reported", "test_type_name",
+                                                    "specimen_type_name", "estimate_grade"])),
         values=fields.List(fields.String())
     )
     aggregation_variable = fields.String()
