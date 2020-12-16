656ddbf4a2c307674d8dcb123fcfd23a03fb3fb8
Austin Atmaja <austin.atmaja@gmail.com>
2020-12-15 20:09:43 -0500

    first author bugfix

--- app/database_etl/etl_main.py
+++ app/database_etl/etl_main.py
@@ -279,13 +279,14 @@ def get_most_recent_publication_info(row):
         row['source_type'] = row['source_type'][max_index]
 
     # Index whether org author exists and corresponding first author
-    is_org_author = row['organizational_author'][max_index]
-    row['organizational_author'] = is_org_author
-    row['first_author'] = row['first_author'][max_index]
+    if row['organizational_author'] and row['first_author']:
+        is_org_author = row['organizational_author'][max_index]
+        row['organizational_author'] = is_org_author
+        row['first_author'] = row['first_author'][max_index]
 
-    # If it is not an organizational author, then get last name
-    if not is_org_author and len(row['first_author']) > 0:
-        row['first_author'] = row['first_author'].strip().split()[-1]
+        # If it is not an organizational author, then get last name
+        if not is_org_author and len(row['first_author']) > 0:
+            row['first_author'] = row['first_author'].strip().split()[-1]
     return row
 
 
