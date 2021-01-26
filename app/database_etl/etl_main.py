bac9e603b8e8af59fe78478b1d061870f01a35d8
Austin Atmaja <austin.atmaja@gmail.com>
2021-01-26 10:36:39 -0500

    Incorporate pr fbk

--- app/database_etl/etl_main.py
+++ app/database_etl/etl_main.py
@@ -450,16 +450,14 @@ def add_country_code_to_state(row):
 # Send alert email if filter options have changed
 def check_filter_options(dashboard_source):
     curr_filter_options = get_filter_static_options()
-    to_ignore = ["All", "Multiple groups", "Multiple populations", "Multiple Types", None]
+    to_ignore = set(["All", "Multiple groups", "Multiple populations", "Multiple Types", None])
     changed_filter_options = {}
 
     for filter_type in curr_filter_options:
         # Get new options for each filter type
         new_options = set(dashboard_source[filter_type].unique())
         # Remove options that are unused (e.g. "All", "Multiple groups", etc)
-        for s in to_ignore:
-            if s in new_options:
-                new_options.remove(s)
+        new_options = set([s for s in new_options if s not in to_ignore])
         # Check to see if the new options are equal to the curr hardcoded options
         if new_options != set(curr_filter_options[filter_type]):
             changed_filter_options[filter_type] = new_options
@@ -601,7 +599,7 @@ def main():
     # key = table name, value = table df
     tables_dict = {**multi_select_tables_dict, **bridge_tables_dict}
     tables_dict['dashboard_source'] = dashboard_source
-    #tables_dict['research_source'] = research_source
+    tables_dict['research_source'] = research_source
     tables_dict['country'] = country_df
 
     # Load dataframes into postgres tables
