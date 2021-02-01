aea910f5ee82e174cea4603a599e11be2e7d426e
simonarocco <serocco@uwaterloo.ca>
2021-01-31 23:18:38 -0500

    writing csvs to s3 buckets

--- app/database_etl/etl_tableau.py
+++ app/database_etl/etl_tableau.py
@@ -1,28 +1,54 @@
-from app.utils import get_filtered_records
-from app.serotracker_sqlalchemy import dashboard_source_cols
-import pandas as pd
 import os
 import logging
 
+from app.utils import get_filtered_records, send_slack_message
+from app.serotracker_sqlalchemy import dashboard_source_cols
+
+import boto3
+import pandas as pd
+from dotenv import load_dotenv
+from botocore.exceptions import ClientError
+
+
+load_dotenv()
+
+
+def upload_s3_file(file_name, bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    try:
+        bucket.upload_file(Filename=file_name, Key=file_name)
+    except ClientError as e:
+        body = f'Error uploading to s3 bucket: {e}'
+        logging.error(body)
+        send_slack_message(body)
+        return
+    return
+
+
 def upload_analyze_csv():
-    records = get_filtered_records(research_fields=False, filters=None, columns=None, start_date=None, end_date=None,
-                                  prioritize_estimates=True)
+    # Get records
+    records = get_filtered_records(research_fields=False, filters=None, columns=None, start_date=None,
+                                   end_date=None, prioritize_estimates=True)
     records_df = pd.DataFrame(records)
-    # create CSV
-    temp_filepath = "analyze_records.csv"
-    records_df.to_csv(temp_filepath, index=False)
+
+    # Create CSV
+    filepath = "tableau_analyze_records.csv"
+    records_df.to_csv(filepath, index=False)
+
     # If file exists
-    if os.path.isfile(temp_filepath):
-        # TODO: upload to S3
-        # delete CSV
-        #os.remove(temp_filepath)
+    if os.path.isfile(filepath):
+        upload_s3_file(filepath, 'tableau-csv-data')
+        os.remove(filepath)
         pass
     else:
         # Send error email
-        logging.error("ERROR: Analyze CSV was not created")
-        # TODO: send slack message with error
-
+        message = "ERROR: Analyze CSV was not created"
+        logging.error(message)
+        send_slack_message(message)
+        return
     return
+
 
 def get_adj_level(row):
     lvl = 0
@@ -32,15 +58,17 @@ def get_adj_level(row):
         lvl += 1
     return lvl
 
+
 def upload_canadian_explore_csv():
-    columns = dashboard_source_cols + ["subgroup_var", "subgroup_specific_category", "pop_adj", "test_adj", "estimate_name"]
+    # Get Canadian records
+    columns =\
+        dashboard_source_cols + ["subgroup_var", "subgroup_specific_category", "pop_adj", "test_adj", "estimate_name"]
     filters = {
         "country": ["Canada"]
     }
-    records = get_filtered_records(research_fields=True, filters=filters, columns=columns, start_date=None, end_date=None,
-                                  prioritize_estimates=False)
+    records = get_filtered_records(research_fields=True, filters=filters, columns=columns,
+                                   start_date=None, end_date=None, prioritize_estimates=False)
     records_df = pd.DataFrame(records)
-
 
     # We only want top level estimates and subgeography estimates, so
     # filter out records with subgroup estimates other than
@@ -50,25 +78,28 @@ def upload_canadian_explore_csv():
 
     # Get the most adjusted estimate for each subgeography
     records_df['adj_level'] = records_df.apply(lambda row: get_adj_level(row), axis=1)
+
     # note: need to fill None columns with a placeholder so that groupby works
     records_df['subgroup_specific_category'] = records_df['subgroup_specific_category'].fillna("None")
     records_df = records_df.loc[records_df.groupby(['study_name', 'subgroup_specific_category'])['adj_level'].idxmax()]
 
-    # create CSV
-    temp_filepath = "canadian_explore_records.csv"
-    records_df.to_csv(temp_filepath, index=False)
+    # Create CSV
+    filepath = "tableau_canadian_explore_records.csv"
+    records_df.to_csv(filepath, index=False)
+
     # If file exists
-    if os.path.isfile(temp_filepath):
-        # TODO: upload to S3
-        # delete CSV
-        #os.remove(temp_filepath)
+    if os.path.isfile(filepath):
+        upload_s3_file(filepath, 'tableau-csv-data')
+        os.remove(filepath)
         pass
     else:
         # Send error email
-        logging.error("ERROR: Canadian Explore CSV was not created")
-        # TODO: send slack message with error
-
+        message = "ERROR: Canadian Explore CSV was not created"
+        logging.error(message)
+        send_slack_message(message)
+        return
     return
+
 
 if __name__ == "__main__":
     upload_canadian_explore_csv()
