34d1f6377abd7baf205fb0cdf50a7cbc00159e72
Abel Joseph <abeljohnjoseph@gmail.com>
2021-02-13 19:45:59 -0500

    Ingest OWID data

---
+++ app/database_etl/owid_ingestion_handler/owid_data_handler.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import datetime
+
+CSV_DIR = "/home/ubuntu/data/"
+
+vaccination_df = pd.read_csv(CSV_DIR + "vaccinations.csv")[['date', 'iso_code', 'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred']]
+tests_df = pd.read_csv(CSV_DIR + "tests.csv")  # per million
+cases_df = pd.read_csv(CSV_DIR + "cases.csv")  # per million
+deaths_df = pd.read_csv(CSV_DIR + "deaths.csv")
+
+
+def get_midpoint(start_date: datetime, end_date: datetime):
+    return start_date + (end_date - start_date) / 2
+
+
+def get_total_cases(country_name: str, midpoint_date: datetime):
+    offset = datetime.timedelta(days=-9)
+    target_date = midpoint_date + offset
+
+    per_million = cases_df[['date', country_name]].loc[cases_df['date'] == target_date][country_name]
+    return float(per_million) / 10000  # per hundred
+
+
+def get_total_tests(country_id: str, midpoint_date: datetime):
+    country_total_tests = tests_df.loc[tests_df['ISO code'] == country_id]
+    per_thousand = country_total_tests.loc[country_total_tests['Date'] == midpoint_date.strftime('%Y-%m-%d')]['Cumulative total per thousand']
+    return float(per_thousand) / 10  # per hundred
+
+
+def get_total_deaths(country_name: str, midpoint_date: datetime):
+    offset = datetime.timedelta(days=4)
+    target_date = midpoint_date + offset
+
+    per_million = deaths_df[['date', country_name]].loc[deaths_df['date'] == target_date][country_name]
+    return float(per_million) / 10000  # per hundred
+
+
+def get_vaccinated(country_id: str, midpoint_date: datetime):
+    offset = datetime.timedelta(days=-14)
+    target_date = midpoint_date + offset
+
+    country_data = vaccination_df.loc[vaccination_df['iso_code'] == country_id]
+    return float(country_data.loc[country_data['date'] == target_date]['people_vaccinated_per_hundred'])
+
+
+def get_fully_vaccinated(country_id: str, midpoint_date: datetime):
+    offset = datetime.timedelta(days=-14)
+    target_date = midpoint_date + offset
+
+    country_data = vaccination_df.loc[vaccination_df['iso_code'] == country_id]
+    return float(country_data.loc[country_data['date'] == target_date]['people_fully_vaccinated_per_hundred'])
+
--- app/database_etl/postgres_tables_handler/table_generator.py
+++ app/database_etl/postgres_tables_handler/table_generator.py
@@ -6,6 +6,7 @@ from app.utils.notifications_sender import send_slack_message
 from app.utils import airtable_fields_config
 from app.database_etl.location_utils import add_latlng_to_df, get_country_code
 from .table_formatter import replace_null_string
+from app.database_etl.owid_ingestion_handler import get_fully_vaccinated, get_vaccinated, get_total_tests, get_total_deaths, get_total_cases, get_midpoint
 
 import pandas as pd
 
@@ -39,6 +40,25 @@ def create_dashboard_source_df(original_data, current_time):
     for col in date_cols:
         original_data[col] = \
             original_data[col].apply(lambda x: datetime.strptime(x, '%Y-%m-%d') if x is not None else x)
+
+    # Create midpoint date column
+    original_data['midpoint'] = original_data.apply(lambda row: get_midpoint(row['sampling_start_date'], row['sampling_end_date']), axis=1)
+
+    # Create total cases column
+    original_data['case_count'] = original_data.apply(lambda row: get_total_cases(row['country_name'], row['midpoint']))
+
+    # Create total tests column
+    original_data['test_count'] = original_data.apply(lambda row: get_total_tests(row['country_id'], row['midpoint']))
+
+    # Create total deaths column
+    original_data['death_count'] = original_data.apply(lambda row: get_total_deaths(row['country_name'], row['midpoint']))
+
+    # Create vaccination count column
+    original_data['vaccination_count'] = original_data.apply(lambda row: get_vaccinated(row['country_id'], row['midpoint']))
+
+    # Create full vaccination count column
+    original_data['full_vaccination_count'] = original_data.apply(lambda row: get_fully_vaccinated(row['country_id'], row['midpoint']))
+
     return original_data
 
 
@@ -108,6 +128,8 @@ def create_multi_select_tables(original_data, current_time):
     # Get countries for each state
     multi_select_tables_dict["state"] = add_latlng_to_df("region", "state_name", multi_select_tables_dict["state"])
     multi_select_tables_dict["city"] = add_latlng_to_df("place", "city_name", multi_select_tables_dict["city"])
+
+    #
 
     # Delete country iso2 column, no longer needed
     # Note: only need this temporarily, so fine to drop
