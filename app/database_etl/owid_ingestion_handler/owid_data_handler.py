2d5715c2df073f084cd1f73a2635102fba6f4c0d
Abel Joseph <abeljohnjoseph@gmail.com>
2021-02-16 20:49:12 -0500

    Use alternative country names from map

--- app/database_etl/owid_ingestion_handler/owid_data_handler.py
+++ app/database_etl/owid_ingestion_handler/owid_data_handler.py
@@ -2,45 +2,64 @@ import pandas as pd
 import datetime
 import os
 
+from app.database_etl.location_utils import get_alternative_names, get_country_code
+
 CSV_DIR = os.getenv('CSV_DIR')
 
 vaccination_df = pd.read_csv(CSV_DIR + "vaccinations.csv")[['date', 'iso_code', 'people_vaccinated_per_hundred', 'people_fully_vaccinated_per_hundred']]
-tests_df = pd.read_csv(CSV_DIR + "tests.csv")  # per million
+tests_df = pd.read_csv(CSV_DIR + "tests.csv")[['Date', 'ISO code', 'Cumulative total per thousand']]  # per thousand
 cases_df = pd.read_csv(CSV_DIR + "cases.csv")  # per million
-deaths_df = pd.read_csv(CSV_DIR + "deaths.csv")
+deaths_df = pd.read_csv(CSV_DIR + "deaths.csv")  # per million
+
+
+def _get_alt_name(country_name, df):
+    country_id = get_country_code(country_name)
+    if country_id is None: return None
+    alt_names = get_alternative_names(country_id)
+    if alt_names is None: return None
+    for name in alt_names:
+        if name in df.columns:
+            return name
+    return None
 
 
 def get_midpoint(start_date: datetime, end_date: datetime):
     return start_date + (end_date - start_date) / 2
 
 
+def get_total_tests(country_name: str, midpoint_date: datetime):
+    country_id = get_country_code(country_name)
+    country_total_tests = tests_df.loc[tests_df['ISO code'] == country_id]
+    per_thousand = country_total_tests.loc[country_total_tests['Date'] == midpoint_date.strftime('%Y-%m-%d')]['Cumulative total per thousand']
+    return float(per_thousand) / 10  # per hundred
+
+
 def get_total_cases(country_name: str, midpoint_date: datetime):
     offset = datetime.timedelta(days=-9)
     target_date = midpoint_date + offset
 
+    if country_name not in cases_df.columns:
+        country_name = _get_alt_name(country_name, cases_df)
+
+    if country_name is None: return None
     per_million = cases_df[['date', country_name]].loc[cases_df['date'] == target_date][country_name]
     return float(per_million) / 10000  # per hundred
-
-
-def get_total_tests(country_name: str, midpoint_date: datetime):
-    # TODO: use helper func to convert name to ISO3
-    country_id = country_name
-    country_total_tests = tests_df.loc[tests_df['ISO code'] == country_id]
-    per_thousand = country_total_tests.loc[country_total_tests['Date'] == midpoint_date.strftime('%Y-%m-%d')]['Cumulative total per thousand']
-    return float(per_thousand) / 10  # per hundred
 
 
 def get_total_deaths(country_name: str, midpoint_date: datetime):
     offset = datetime.timedelta(days=4)
     target_date = midpoint_date + offset
 
+    if country_name not in deaths_df.columns:
+        country_name = _get_alt_name(country_name, deaths_df)
+
+    if country_name is None: return None
     per_million = deaths_df[['date', country_name]].loc[deaths_df['date'] == target_date][country_name]
     return float(per_million) / 10000  # per hundred
 
 
 def get_vaccinated(country_name: str, midpoint_date: datetime):
-    # TODO: use helper func to convert name to ISO3
-    country_id = country_name
+    country_id = get_country_code(country_name)
     offset = datetime.timedelta(days=-14)
     target_date = midpoint_date + offset
 
@@ -49,8 +68,7 @@ def get_vaccinated(country_name: str, midpoint_date: datetime):
 
 
 def get_fully_vaccinated(country_name: str, midpoint_date: datetime):
-    # TODO: use helper func to convert name to ISO3
-    country_id = country_name
+    country_id = get_country_code(country_name)
     offset = datetime.timedelta(days=-14)
     target_date = midpoint_date + offset
 
