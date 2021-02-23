13347f1823c8fb8cb868f5ebbd243bee2676bd58
PRANNOY LAL <prannoylal@gmail.com>
2021-02-23 01:41:54 -0500

    updated init

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,5 +3,5 @@ from .notifications_sender import send_api_error_slack_notif, send_slack_message
 from .cached_json_handler import write_to_json, read_from_json
 from .helper_funcs import validate_request_input_against_schema, convert_start_end_dates
 from .airtable_fields_config import airtable_fields_config, full_airtable_fields
-from .get_filtered_records import get_filtered_records, get_paginated_records
+from .get_filtered_records import get_filtered_records, get_paginated_records_list, get_paginated_records_dict
 from .estimate_prioritization import get_prioritized_estimates
