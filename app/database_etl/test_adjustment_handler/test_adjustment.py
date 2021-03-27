32a02da3d1eb017ce285bb4aa9c3cdd428980045
simonarocco <serocco@uwaterloo.ca>
2021-03-27 13:40:57 -0400

    code review changes

--- app/database_etl/test_adjustment_handler/test_adjustment.py
+++ app/database_etl/test_adjustment_handler/test_adjustment.py
@@ -1,17 +1,17 @@
 # monte-carlo version of RG estimator,
 # accounting for uncertainty in se and sp estimates
+import pickle
 from math import log
-from statsmodels.stats.proportion import proportion_confint
+from hashlib import md5
+from typing import Dict, Tuple, Union
+
 import pandas as pd
 import pystan
 import arviz
-
-import pickle
-from hashlib import md5
-
-from app.utils import get_filtered_records
-from app.database_etl.test_adjustment_handler import bastos_estimates, testadj_model_code
+from statsmodels.stats.proportion import proportion_confint
 from marshmallow import Schema, fields, ValidationError
+
+from app.database_etl.test_adjustment_handler import bastos_estimates, testadj_model_code
 
 
 def validate_against_schema(input_payload, schema):
@@ -34,8 +34,7 @@ class ModelParamsSchema(Schema):
 class TestAdjHandler:
     def __init__(self, model_code=testadj_model_code, model_name='testadj_binomial_se_sp',
                  execution_params={}):
-        self.TESTADJ_MODEL = self.StanModel_cache(model_code=model_code,
-                                    model_name=model_name)
+        self.TESTADJ_MODEL = self.get_stan_model_cache(model_code=model_code, model_name=model_name)
         self.TESTADJ_MODEL_NAME = model_name
         # Parse execution params
         self.n_iter = execution_params.get('n_iter', 2000)
@@ -47,31 +46,31 @@ class TestAdjHandler:
 
     # make sure to gitignore model caches, because the model needs to be compiled separately
     # on each machine - it is system-specific C++ code
-    def StanModel_cache(self, model_code, model_name = 'anon_model', **kwargs):
+    def get_stan_model_cache(self, model_code: str, model_name: str = 'anon_model', **kwargs: Dict) -> pystan.StanModel:
         """Use just as you would `pystan.StanModel`"""
 
+        # Create filepath of cached model
         code_hash = md5(model_code.encode('ascii')).hexdigest()
         cache_fn = f'stanmodelcache-{model_name}-{code_hash}.pkl'
+
+        # Try to load cached model
         try:
-            sm = pickle.load(open(cache_fn, 'rb'))
+            cached_model = pickle.load(open(cache_fn, 'rb'))
             print(f"Using cached StanModel at filepath {cache_fn}")
-        except:
-            sm = pystan.StanModel(model_code = model_code,
-                                         model_name = model_name,
-                                         **kwargs)
+
+        # Otherwise create the model and cache it
+        except FileNotFoundError:
+            cached_model = pystan.StanModel(model_code=model_code, model_name=model_name, **kwargs)
             with open(cache_fn, 'wb') as f:
-                pickle.dump(sm, f)
+                pickle.dump(cached_model, f)
+        return cached_model
 
-        return pystan.StanModel(model_code = model_code,
-                                         model_name = model_name,
-                                         **kwargs)
-
-    def fit_one_pystan_model(self, model_params):
+    def fit_one_pystan_model(self, model_params: Dict) -> Tuple:
         fit = self.TESTADJ_MODEL.sampling(data=model_params,
-                                     iter=self.n_iter,
-                                     chains=self.n_chains,
-                                     control={'adapt_delta': 0.95},
-                                     check_hmc_diagnostics=False)
+                                          iter=self.n_iter,
+                                          chains=self.n_chains,
+                                          control={'adapt_delta': 0.95},
+                                          check_hmc_diagnostics=False)
 
         summary = fit.summary()
         summary_df = pd.DataFrame(data=summary['summary'],
@@ -87,10 +86,9 @@ class TestAdjHandler:
         diagnostics = pystan.diagnostics.check_hmc_diagnostics(fit)
 
         satisfactory_model_found = all(diagnostics.values())
-
         return summary_df_parsed, satisfactory_model_found
 
-    def pystan_adjust(self, model_params, execution_params={}):
+    def pystan_adjust(self, model_params: Dict, execution_params: Dict = {}) -> Union[Tuple, pystan.StanModel]:
         if self.n_replicates % 2 != 1:
             raise ValueError(f'n_replicates must be odd')
         credible_interval_size = execution_params.get('credible_interval_size', 0.95)
@@ -151,7 +149,7 @@ class TestAdjHandler:
         else:
             return lower, median, upper
 
-    def get_adjusted_estimate(self, estimate):
+    def get_adjusted_estimate(self, estimate: pd.Series) -> Tuple:
         # initialize adj_type
         adj_type = None
 
@@ -164,13 +162,14 @@ class TestAdjHandler:
                 # Also note these must be divided by 100
                 se = (estimate['ind_se']) / 100
                 sp = (estimate['ind_sp']) / 100
-                se_n = float(estimate['ind_se_n'][0])*100 if estimate['ind_se_n'] is not None else 30
-                sp_n = float(estimate['ind_sp_n'][0])*100 if estimate['ind_sp_n'] is not None else 80
+                se_n = float(estimate['ind_se_n'][0]) * 100 if estimate['ind_se_n'] is not None else 30
+                sp_n = float(estimate['ind_sp_n'][0]) * 100 if estimate['ind_sp_n'] is not None else 80
 
             # Author evaluation is available
             elif pd.notna(estimate['se_n']) and pd.notna(estimate['sp_n']) and \
                     pd.notna(estimate['sensitivity']) and pd.notna(estimate['specificity']):
-                if estimate['test_validation'] and 'Validated by independent authors/third party/non-developers' in estimate['test_validation']:
+                if estimate['test_validation'] and \
+                        'Validated by independent authors/third party/non-developers' in estimate['test_validation']:
                     adj_type = 'Author-reported independent evaluation'
                 else:
                     adj_type = 'Test developer / manufacturer evaluation'
@@ -184,7 +183,9 @@ class TestAdjHandler:
                 adj_type = 'Test developer / manufacturer evaluation'
                 se = estimate['sensitivity']
                 sp = estimate['specificity']
-                # per FDA minimum requirements https://www.fda.gov/medical-devices/coronavirus-disease-2019-covid-19-emergency-use-authorizations-medical-devices/eua-authorized-serology-test-performance
+                # per FDA minimum requirements https://www.fda.gov/medical-devices/
+                # coronavirus-disease-2019-covid-19-emergency-use-authorizations-medical-devices/
+                # eua-authorized-serology-test-performance
                 se_n, sp_n = 30, 80
 
             else:
@@ -234,67 +235,12 @@ class TestAdjHandler:
         return adj_prev, se, sp, adj_type, lower, upper
 
 
-def test_code(model_code=testadj_model_code,
-              model_name='testadj_binomial_se_sp'):
-    # use santa clara and denmark findings as sample data
-    bendavid_data = {
-        'n_prev_obs': 3330,
-        'y_prev_obs': 50,
-        'n_se': 122,
-        'y_se': 103,
-        'n_sp': 401,
-        'y_sp': 399
-    }
-
-    denmark_data = {
-        'n_prev_obs': 20640,
-        'y_prev_obs': 413,
-        'n_se': 59,
-        'y_se': 40,
-        'n_sp': 14,
-        'y_sp': 9
-    }
-    testadjHandler = TestAdjHandler(model_code=model_code, model_name=model_name)
-    return {
-        "bendavid_data": testadjHandler.pystan_adjust(bendavid_data),
-        "denmark_data": testadjHandler.pystan_adjust(denmark_data)
-    }
-
-
-def run_on_test_set(model_code=testadj_model_code, model_name='testadj_binomial_se_sp'):
+def run_on_test_set(model_code: str = testadj_model_code, model_name: str = 'testadj_binomial_se_sp') -> pd.DataFrame:
     records_df = pd.read_csv('test_adj_test_set.csv')
     testadjHandler = TestAdjHandler(model_code=model_code, model_name=model_name)
+
     # Write to csv
     records_df['adj_prevalence'], records_df['adj_sensitivity'], records_df['adj_specificity'], \
     records_df['ind_eval_type'], records_df['adj_prev_ci_lower'], records_df['adj_prev_ci_upper'] = \
         zip(*records_df.apply(lambda row: testadjHandler.get_adjusted_estimate(row), axis=1))
-
     return records_df
-
-
-if __name__ == '__main__':
-    # To resolve error when running multiple chains at once:
-    # https://discourse.mc-stan.org/t/new-to-pystan-always-get-this-error-when-attempting-to-sample-modulenotfounderror-no-module-named-stanfit4anon-model/19288/3
-
-    # Get records
-    records = get_filtered_records(research_fields=True, filters=None, columns=None,
-                                   sampling_start_date=None, sampling_end_date=None,
-                                   prioritize_estimates=False)
-    records_df = pd.DataFrame(records)
-
-    # Turn lists into comma sep strings
-    cols = ['city', 'state', 'test_manufacturer', 'antibody_target', 'isotypes_reported']
-    for col in cols:
-        records_df[col] = records_df[col].apply(lambda x: ",".join(x))
-
-    # Clean df
-    records_df['source_id'] = records_df['source_id'].apply(lambda x: str(x))
-    records_df = records_df.replace('[', '')
-    records_df = records_df.replace(']', '')
-
-    testadjHandler = TestAdjHandler()
-    # Write to csv
-    records_df['adj_prevalence'], records_df['adj_sensitivity'], records_df['adj_specificity'], \
-    records_df['ind_eval_type'], records_df['adj_prev_ci_lower'], records_df['adj_prev_ci_upper'] = \
-        zip(*records_df.apply(lambda row: testadjHandler.get_adjusted_estimate(row), axis=1))
-    print("COMPLETED")
--- app/database_etl/test_adjustment_handler/test_adjustment_constants.py
+++ app/database_etl/test_adjustment_handler/test_adjustment_constants.py
@@ -127,34 +127,6 @@ model {
 }
 """
 
-# uniform_sens_spec_model_code = """
-# data {
-#     int<lower = 0> n_prev_obs;
-#     int<lower = 0> y_prev_obs;
-#     int<lower = 0> n_se;
-#     int<lower = 0> y_se;
-#     int<lower = 0> n_sp;
-#     int<lower = 0> y_sp;
-# }
-# transformed data {
-#     real sens_reported = y_se * 1.0 / n_se;
-#     real spec_reported = y_sp * 1.0 / n_sp;
-# }
-# parameters {
-#     real<lower = 0, upper = 1> prev;
-#     real<lower = 0, upper = 1> sens;
-#     real<lower = 0, upper = 1> spec;
-# }
-# model {
-#     real prev_obs;
-#
-#     prev_obs = prev * sens + (1 - prev) * (1 - spec);
-#     y_prev_obs ~ binomial(n_prev_obs, prev_obs);
-#     sens ~ uniform(sens_reported - 0.01, sens_reported + 0.01);
-#     spec ~ uniform(spec_reported - 0.01, spec_reported + 0.01);
-# }
-# """
-
 jeffries_fixed_sens_spec_model_code = """
 data {
     int<lower = 0> n_prev_obs;
