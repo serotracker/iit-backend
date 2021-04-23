f234b32d05ecd86982a51112847004e2dfe8c8df
Rahul Arora <rahularoradfs@gmail.com>
2021-04-23 10:22:54 -0600

    lint estimate prioritization

--- app/utils/estimate_prioritization/estimate_prioritization.py
+++ app/utils/estimate_prioritization/estimate_prioritization.py
@@ -17,7 +17,7 @@ def get_pooled_estimate(estimates: pd.DataFrame) -> pd.DataFrame:
 
         # for each variable that we have a defined pooling function for
         # attempt to generate a pooled value for this variable by applying the pooling function to the input data
-        for (summary_type, cols_to_summarize, summary_function) in pooling_function_maps:
+        for (_, cols_to_summarize, summary_function) in pooling_function_maps:
             if summary_function is not None:
                 # check to ensure that the value to be pooled is in the data input provided 
                 valid_cols_to_summarize = set(cols_to_summarize) & set(estimates.columns)
@@ -44,7 +44,7 @@ def get_pooled_estimate(estimates: pd.DataFrame) -> pd.DataFrame:
                                     nobs = pooled.at['denominator_value'], alpha = 0.05, method = 'jeffreys')
             else:
                 pooled.at['adj_prev_ci_lower'], pooled.at['adj_prev_ci_upper'] = pd.NA, pd.NA
-        except KeyError as e: 
+        except KeyError: 
             pass
 
     return pooled
@@ -72,7 +72,7 @@ def get_prioritized_estimates(estimates: pd.DataFrame,
         prioritization_criteria = None
     
     selected_estimates = []
-    for study_name, study_estimates in estimates.groupby('study_name'):
+    for _, study_estimates in estimates.groupby('study_name'):
             
         # if there is only one estimate, use that
         if study_estimates.shape[0] == 1:
@@ -86,7 +86,7 @@ def get_prioritized_estimates(estimates: pd.DataFrame,
 
             try:
                 frac_test_info_avail = study_estimates['adj_prevalence'].notna().sum() / study_estimates.shape[0]
-            except KeyError as e:
+            except KeyError:
                 frac_test_info_avail = 0
 
             if frac_test_info_avail >= 0.65:
@@ -100,7 +100,7 @@ def get_prioritized_estimates(estimates: pd.DataFrame,
             # if a prioritization criterion yields multiple estimates, continue applying criteria 
 
         # select a criterion
-        for criterion_name, criterion in prioritization_criteria.items():
+        for _, criterion in prioritization_criteria.items():
             # go through the levels of that criterion, from highest to lowest
             for level in criterion:
                 study_estimates_at_level = study_estimates[study_estimates.apply(level, axis = 1)]
