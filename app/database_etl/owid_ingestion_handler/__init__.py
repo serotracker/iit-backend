240787700fdde10432cd7e12869c6c16f3bdbdc5
simonarocco <serocco@uwaterloo.ca>
2021-05-02 23:08:10 -0400

    adding vaccination policy column

--- app/database_etl/owid_ingestion_handler/__init__.py
+++ app/database_etl/owid_ingestion_handler/__init__.py
@@ -1,1 +1,2 @@
-from .owid_data_handler import get_cases_per_hundred, get_deaths_per_hundred, get_tests_per_hundred, get_vaccinations_per_hundred, get_midpoint, get_whether_exact_match
+from .owid_data_handler import get_cases_per_hundred, get_deaths_per_hundred, get_tests_per_hundred, \
+    get_vaccinations_per_hundred, get_vaccination_policy, get_midpoint, get_whether_exact_match
