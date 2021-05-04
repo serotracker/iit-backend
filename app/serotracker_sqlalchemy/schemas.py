26c3ae0797ca7f62bd88d863de655c4057c74e42
Rahul Arora <rahularoradfs@gmail.com>
2021-05-04 09:28:59 -0600

    other file changes

--- app/serotracker_sqlalchemy/schemas.py
+++ app/serotracker_sqlalchemy/schemas.py
@@ -57,6 +57,7 @@ class ResearchSourceSchema(Schema):
     death_count_plus11 = fields.Float(allow_none=True, allow_nan=True)
     death_count_plus4 = fields.Float(allow_none=True, allow_nan=True)
     include_in_srma = fields.Boolean(allow_none=True)
+    sensspec_from_manufacturer = fields.Boolean(allow_none=True)
     ind_eval_lab = fields.Str(allow_none=True)
     ind_eval_link = fields.Str(allow_none=True)
     ind_se = fields.Float(allow_none=True, allow_nan=True)
