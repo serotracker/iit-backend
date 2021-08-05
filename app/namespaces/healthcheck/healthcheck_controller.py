53ef039ba0d501e7139a0d9d2d6cc7e7f21a2802
Austin Atmaja <austin.atmaja@gmail.com>
2021-08-05 12:47:27 -0400

    reverting changes to the healthcheck endpoint as another test

--- app/namespaces/healthcheck/healthcheck_controller.py
+++ app/namespaces/healthcheck/healthcheck_controller.py
@@ -8,5 +8,5 @@ healthcheck_ns = Namespace('healthcheck', description='A health check endpoint.'
 class Healthcheck(Resource):
     @healthcheck_ns.doc('A health check endpoint.')
     def get(self):
-        response = 'The healthcheck endpoint was hit, nice!'
+        response = 'The healthcheck endpoint was hit.'
         return jsonify(response)
