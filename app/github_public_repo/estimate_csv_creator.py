e73ca8ba3f1233223167f51952a32bac466bcee9
prannoy lal <prannoylal@gmail.com>
2021-11-03 18:48:14 -0400

    print path

--- app/github_public_repo/estimate_csv_creator.py
+++ app/github_public_repo/estimate_csv_creator.py
@@ -45,12 +45,14 @@ try:
     # Sort estimates by country, publication date, source name, study name, primary estimates first
     csv_records_df.sort_values(by=['country', 'publication_date', 'source_name',
                                    'study_name', 'dashboard_primary_estimate'], inplace=True)
-
     # Save as csv
     abs_filepath_curr_dir = os.getcwd()
     proj_root_abs_path = abs_filepath_curr_dir.split("iit-backend")[0]
+   
     csv_records_df.to_csv(f'{proj_root_abs_path}iit-backend/iit-backend/app/github_public_repo/serotracker_dataset.csv',
                           index=False)
+    print('CONTENTS:')
+    print(os.listdir(f'{proj_root_abs_path}/iit-backend/iit-backend/app/github_public_repo/'))           
 
 except KeyError as e:
     logging.error(f"Failed to retrieve field names and load estimates. Error: {e}")
