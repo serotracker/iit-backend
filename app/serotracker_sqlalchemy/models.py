59ee3cc0e626cbd6c034b2dda0daf98ec25f490b
simonarocco <#19992022Engineer>
2021-11-04 10:42:18 -0400

    removing immunoassays column

--- app/serotracker_sqlalchemy/models.py
+++ app/serotracker_sqlalchemy/models.py
@@ -82,7 +82,6 @@ class ResearchSource(db.Model):
     death_count_plus4 = Column(Integer)
     include_in_srma = Column(Boolean)
     sensspec_from_manufacturer = Column(Boolean)
-    immunoassays = Column(String())
     ind_eval_lab = Column(String())
     ind_eval_link = Column(String())
     ind_se = Column(Float)
