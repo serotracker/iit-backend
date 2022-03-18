4b4420295a91d13402120d2f27945355f082e48a
simonarocco <#19992022Engineer>
2022-03-18 15:04:44 -0400

    fixing csv for github and biblio

--- app/github_public_repo/estimate_csv_creator.py
+++ app/github_public_repo/estimate_csv_creator.py
@@ -4,6 +4,7 @@ import pandas as pd
 from flask import current_app as app
 
 from app.database_etl.airtable_records_handler import get_all_records, airtable_get_request
+from app.database_etl.airtable_records_handler.airtable_records_formatter import replace_null_fields
 
 
 # Get use case of script from environment variable
@@ -38,17 +39,30 @@ try:
     # Convert to df
     csv_records_df = pd.DataFrame.from_dict(csv_records)
 
-    # For cols where values are in lists, extract from list
-    multi_val_cols = ['isotypes', 'antibody_target', 'source_type', 'jbi_1', 'jbi_2', 'jbi_3', 'jbi_4', 'jbi_5',
-                      'jbi_6', 'jbi_7', 'jbi_8', 'jbi_9', 'source_name', 'lead_institution', 'url', 'first_author',
-                      'is_unity_aligned', 'publication_date', 'study_type', 'study_inclusion_criteria',
-                      'study_exclusion_criteria', 'alpha_3_code']
-    for col in multi_val_cols:
-        csv_records_df[col] = csv_records_df[col].apply(lambda x: x[0] if x is not None else x)
+    # List of columns that are lookup fields and therefore only have one element in the list
+    single_element_list_cols = ['source_type', 'jbi_1', 'jbi_2', 'jbi_3', 'jbi_4',
+                                'jbi_5', 'jbi_6', 'jbi_7', 'jbi_8', 'jbi_9', 'source_name', 'lead_institution', 'url',
+                                'first_author', 'is_unity_aligned', 'publication_date', 'study_type',
+                                'study_inclusion_criteria', 'study_exclusion_criteria', 'alpha_3_code',
+                                'zotero_citation_key']
+    csv_records_df_cols = csv_records_df.columns.values.tolist()
+    for col in single_element_list_cols:
+        # Check if the col exists in the dataframe because the cols pulled for github and biblio differ
+        if col in csv_records_df_cols:
+            csv_records_df[col] = csv_records_df[col].apply(lambda x: x[0] if x is not None else x)
 
     # Convert elements that are "Not reported" or "Not Reported" or "NR" to None
     csv_records_df.replace({'nr': None, 'NR': None, 'Not Reported': None, 'Not reported': None,
                             'Not available': None, 'NA': None, 'N/A': None}, inplace=True)
+
+    # Antibody target and isotypes are multi select and will have ['Not reported'], ['NR'] - convert to None
+    # Can also have a situation with ["Spike", "Not reported"] so need to check within list
+    csv_records_df['antibody_target'] = csv_records_df['antibody_target'].apply(lambda x: replace_null_fields(x))
+    csv_records_df['isotypes'] = csv_records_df['isotypes'].apply(lambda x: replace_null_fields(x))
+
+    # If antibody_target and isotypes end up only having one element, remove from list
+    for col in ['antibody_target', 'isotypes']:
+        csv_records_df[col] = csv_records_df[col].apply(lambda x: x[0] if len(x) == 1 else x)
 
     # Reorder df columns based on airtable order
     csv_records_df = csv_records_df[snake_case_col_name]
