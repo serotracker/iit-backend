d87e41a4eaf25a3b7e2ec7a715e6188f8d185d18
Gurman Brar <gunitbrar@Gunits-MacBook-Pro.local>
2023-07-09 15:56:36 -0400

    Auto set sqlalchemy URL based on .env vars

--- Pathogens/Arbo/migrations/env.py
+++ Pathogens/Arbo/migrations/env.py
@@ -7,6 +7,10 @@ from alembic import context
 
 from app.sqlalchemy import Base
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,6 +24,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
+#Set the DB url form .env file
+print(f"Using database url: {os.environ.get('DATABASE_URL')}")
+config.set_main_option(
+    'sqlalchemy.url',
+    os.environ.get('DATABASE_URL')
+)
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
