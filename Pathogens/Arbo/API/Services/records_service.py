683b9137be682875d5833b899cd8a07ab512ecef
HimanshuRanka <himanshuranka2612@gmail.com>
2023-08-30 17:37:10 -0400

    Updated ETL to populate the antibody table

--- Pathogens/Arbo/API/Services/records_service.py
+++ Pathogens/Arbo/API/Services/records_service.py
@@ -1,25 +1,36 @@
-from sqlalchemy import select, distinct
+from sqlalchemy import select, distinct, func
 from sqlalchemy.orm import Session
 
-from Pathogens.Arbo.app.sqlalchemy.sql_alchemy_base import Estimate, db_engine
+from Pathogens.Arbo.app.sqlalchemy.sql_alchemy_base import Estimate, db_engine, Antibody, AntibodyToEstimate
 
 
 def get_all_arbo_records():
     with Session(db_engine) as session:
-        records = session.query(Estimate).all()
+        records = session.query(Estimate, func.array_agg(Antibody.antibody).label("antibodies")).\
+            join(AntibodyToEstimate, Estimate.id == AntibodyToEstimate.estimate_id).\
+            join(Antibody, Antibody.id == AntibodyToEstimate.antibody_id).\
+            group_by(Estimate.id).all()
+
+        print(f'[DEBUG] estimate record: {records[0]}')
 
         # Convert records to a list of dictionaries
-        record_dicts = [record.__dict__ for record in records]
+        result_list = []
+        for estimate, antibodies in records:
+            record_dict = estimate.__dict__  # Convert Estimate object to a dictionary
+            record_dict['antibodies'] = antibodies  # Add the 'antibodies' key
+            result_list.append(record_dict)
 
         # Remove unnecessary attributes from the dictionaries
-        for record_dict in record_dicts:
+        for record_dict in result_list:
             record_dict.pop("_sa_instance_state")
 
         # Convert the list of dictionaries to JSON
         # Print or do whatever you want with the JSON data
-        print(record_dicts)
+        for record in result_list[0:5]:
+            print(f'[DEBUG] estimate record: {record}')
+            print("--------------------")
 
-        return record_dicts
+        return result_list
 
 
 def get_all_arbo_filter_options():
@@ -30,6 +41,10 @@ def get_all_arbo_filter_options():
         country = session.query(distinct(Estimate.country)).all()
         producer = session.query(distinct(Estimate.producer)).all()
         sample_frame = session.query(distinct(Estimate.sample_frame)).all()
+        antibody = session.query(distinct(Antibody.antibody)).all()
+
+        print(f'[DEBUG] age_group: {age_group[0]}')
+        print(f'[DEBUG] antibody: {antibody[0]}')
 
         result_dict = {
             "age_group": [item[0] for item in age_group],
@@ -37,7 +52,8 @@ def get_all_arbo_filter_options():
             "assay": [item[0] for item in assay],
             "country": [item[0] for item in country],
             "producer": [item[0] for item in producer],
-            "sample_frame": [item[0] for item in sample_frame]
+            "sample_frame": [item[0] for item in sample_frame],
+            "antibody": [item[0] for item in antibody]
         }
 
         return result_dict
--- Pathogens/Arbo/ETL/arbo_etl_main.py
+++ Pathogens/Arbo/ETL/arbo_etl_main.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import Session
 
 from Pathogens.Arbo.ETL.constants import estimate_columns, single_select_lists
 from Pathogens.Arbo.app.sqlalchemy import db_engine
-from Pathogens.Arbo.app.sqlalchemy.sql_alchemy_base import Estimate
+from Pathogens.Arbo.app.sqlalchemy.sql_alchemy_base import Estimate, Antibody, AntibodyToEstimate
 from Pathogens.Utility.location_utils.location_functions import get_lng_lat
 
 AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
@@ -32,6 +32,45 @@ def flatten_single_select_lists(x):
         return x[0]
     else:
         return x
+
+
+def load_into_db(data, table_name, schema='arbo'):
+    try:
+        data.to_sql(table_name,
+                    schema=schema,
+                    con=db_engine,
+                    if_exists='append',
+                    index=False)
+        print("[DB] " + table_name + " data uploaded to database")
+        return True
+    except (SQLAlchemyError, ValueError) as e:
+        # Send slack error message
+        body = f'[ERROR] Error occurred while loading {table_name} data into Postgres: {e}: '
+        print(body)
+        return False
+
+
+def load_antibody_data_into_db(data):
+    try:
+        for antibody_pair in data.itertuples():
+            estimate_id = antibody_pair[1]
+            antibodies = antibody_pair[2]
+            with Session(db_engine) as session:
+                for antibody_name in antibodies:
+                    if antibody_name is not None:
+                        antibody = session.query(Antibody).filter_by(antibody=antibody_name).first()
+                        if antibody is None:
+                            antibody = Antibody(id=uuid.uuid4(), antibody=antibody_name, created_at=CURR_TIME)
+                            session.add(antibody)
+                            session.commit()
+                        antibody_estimate_relationship = AntibodyToEstimate(id=uuid.uuid4(), antibody_id=antibody.id,
+                                                                            estimate_id=estimate_id, created_at=CURR_TIME)
+                        session.add(antibody_estimate_relationship)
+                session.commit()
+        return True
+    except SQLAlchemyError as e:
+        print(f'[ERROR] Error occurred while loading antibody data into Postgres: {e} ')
+        return False
 
 
 def main():
@@ -81,21 +120,21 @@ def main():
 
     records_df['id'] = [uuid.uuid4() for _ in range(len(records_df))]
 
+    # flatten out single select lists
     for col in single_select_lists:
         records_df[col] = records_df[col].apply(lambda x: x[0] if x is not None else x)
-
-    print(records_df.columns)
-    print(records_df.head(20).to_string())
 
     # Convert the publication, sampling start date and sampling end date to datetime
     date_cols = ['sample_start_date', 'sample_end_date']
     for col in date_cols:
-        print("Converting date string to datetime object for " + col)
+        print("[STEP] Converting date string to datetime object for " + col)
         records_df[col] = records_df[col].apply(parse_date_cols)
 
     print(records_df.dtypes)
 
+    print("[STEP] generating lat and lng values")
     # calculate the lat and lng values for each of the rows.
+    # TODO: Not working correctly yet
     records_df[['latitude', 'longitude']] = records_df.apply(lambda row:
                                                              pd.Series(get_lng_lat(
                                                                  ','.join(filter(None, [row['city'], row['state']])),
@@ -109,24 +148,27 @@ def main():
                                                                  get_lng_lat(row['country'], 'country'),
                                                                  dtype='object')), axis=1)
 
-    db_loaded_successfully = False
+    dbs_loaded_successfully = True
+    print("[STEP] loading estimate data into database")
+    dbs_loaded_successfully = load_into_db(data=records_df[estimate_columns], table_name='estimate') and dbs_loaded_successfully
 
-    try:
-        records_df[estimate_columns].to_sql("estimate",
-                                            schema='arbo',
-                                            con=db_engine,
-                                            if_exists='append',
-                                            index=False)
-        print("Completed running... Verify in database...")
-        db_loaded_successfully = True
-    except (SQLAlchemyError, ValueError) as e:
-        # Send slack error message
-        body = f'Error occurred while loading tables into Postgres: {e}'
-        print(body)
+    # Replace antibody values with the keys to their the values in their tables
+    # If value does not exist in the table then add it into the table and use the new id
+    print("[STEP] loading antibody data into database")
+    dbs_loaded_successfully = load_antibody_data_into_db(records_df[['id', 'antibody']]) and dbs_loaded_successfully
 
-    if db_loaded_successfully:
+    if dbs_loaded_successfully:
+        print("[STEP] deleting old data from database")
         with Session(db_engine) as session:
-            session.query(Estimate).filter(or_(table.created_at != CURR_TIME, table.created_at.is_(None))).delete()
+            session.query(AntibodyToEstimate).filter(AntibodyToEstimate.created_at != CURR_TIME).delete()
+            session.query(Estimate).filter(Estimate.created_at != CURR_TIME).delete()
+            session.commit()
+    else:
+        print("[ERROR] Issue above deleting new data from database")
+        with Session(db_engine) as session:
+            session.query(AntibodyToEstimate).filter(AntibodyToEstimate.created_at == CURR_TIME).delete()
+            session.query(Estimate).filter(Estimate.created_at == CURR_TIME).delete()
+            session.commit()
 
 
 if __name__ == '__main__':
